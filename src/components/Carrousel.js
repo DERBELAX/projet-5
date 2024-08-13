b50b986f9a4f2fc71105f794f508ee8bea21b029
Marwa Derbel <marwaderbel21@gmail.com>
2024-08-13 11:11:08 +0100

    modif carrousel

--- src/components/Carrousel.js
+++ src/components/Carrousel.js
@@ -1,39 +1,31 @@
-import React, { useState, useEffect, useRef} from 'react';
+import React, { useState} from 'react';
 import rightArrow from '../assets/arrow/arrow-right.png';
 import leftArrow from '../assets/arrow/arrow-left.png';
 import "../style/Carrousel.scss";
 
 function Carrousel({ pictures }) {
   const [currentPicture, setCurrentPicture] = useState(0);
-  const carrouselRef = useRef();
+  ;
 
   const changePicture = (direction) => {
     if (direction === 'next') {
-      setCurrentPicture((prevPicture) => (prevPicture + 1) % pictures.length);
+      if(currentPicture === pictures.length -1){
+        setCurrentPicture(0)
+      } else {
+        setCurrentPicture(currentPicture+1)
+      }
+   
     } else if (direction === 'previous') {
-      setCurrentPicture((prevPicture) =>
-        prevPicture === 0 ? pictures.length - 1 : prevPicture - 1
-      );
+      if(currentPicture === 0)
+        {
+        setCurrentPicture(pictures.length -1)
+      } else {
+        setCurrentPicture(currentPicture -1)
+      }
     }
   };
-
-  useEffect(() => {
-    const handleKeyDown = (event) => {
-      if (event.key === 'ArrowRight') {
-        changePicture('next');
-      } else if (event.key === 'ArrowLeft') {
-        changePicture('previous');
-      }
-    };
-
-    window.addEventListener('keydown', handleKeyDown);
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    };
-  }, [pictures.length]);
-
   return (
-    <div className="carrousel-container" ref={carrouselRef}>
+    <div className="carrousel-container">
       {pictures.length > 1 && (
         <>
           <div
